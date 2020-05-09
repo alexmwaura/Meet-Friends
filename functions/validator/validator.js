1b3f9ff7dabc2563b2e5d34d5bbc57507d1e26e1
alexmwaura <alexmwaura43@gmail.com>
2020-05-10 02:50:14 +0300

    modify

--- functions/validator/validator.js
+++ functions/validator/validator.js
@@ -1,38 +1,30 @@
-
 const isEmpty = (string) => {
-	if (string.trim() === '') return true;
-	else return false;
+  if (string.trim() === "") return true;
+  else return false;
 };
 
 // post validators
 exports.validatePostData = (data) => {
+  let errors = {};
 
-	let errors = {};
-	if (isEmpty(data.author)) {
-		errors.author = 'Must not be empty';
-    }
-    if (isEmpty(data.title)) {
-		errors.title = 'Must not be empty';
-    }
-    if (isEmpty(data.post)) {
-		errors.post = 'Must not be empty';
-	}
-	
-    return {
-        errors,
-        valid: Object.keys(errors).length === 0 ? true:false
-    }
+  if (isEmpty(data.author)) errors.author = "Must not be empty";
+  if (isEmpty(data.title)) errors.title = "Must not be empty";
+  if (isEmpty(data.post)) errors.post = "Must not be empty";
 
-}
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
 
 // post reducers
 exports.postReducers = (data) => {
-    let postData = {}
-    if (!isEmpty(data.category))postData.category= data.category
-    if (data.website.trim().substring(0,4)){
-		postData.website = `http://${data.website.trim()}`
-    }else postData.website = data.website
-    return postData
+  let postData = {};
 
+  if (!isEmpty(data.category)) postData.category = data.category;
+  if (data.website.trim().substring(0, 4))
+    postData.website = `http://${data.website.trim()}`;
+  postData.website = data.website;
 
-}
+  return postData;
+};

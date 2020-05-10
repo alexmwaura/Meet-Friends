f2772ceb061108e957d50625c09c2d2431452a87
alexmwaura <alexmwaura43@gmail.com>
2020-05-10 13:31:00 +0300

    add delete, comment,unlike & like post routes

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,22 @@
 const functions = require("firebase-functions")
 const app = require("express")()
-const {postQuote,getPosts,updatePost,postImage} = require("./posts/posts")
+const {postQuote,getPosts,updatePost,postImage,commentOnPost,deletePost,likePost,unlikePost} = require("./posts/posts")
+const {signUp,login} = require("./users/users")
+const AuthMiddleware = require("./middleware/Auth")
 
-// post request
-app.post("/post", postQuote)
-app.post("/post/:postId",updatePost)
-app.post("/post/image/:postId", postImage)
-//  get request
+// authentication routes
+app.post("/signup",signUp)
+app.post("/login",login)
+
+// posts routes
+app.post("/post", AuthMiddleware,postQuote)
+app.post("/post/:postId", AuthMiddleware,updatePost)
+app.post("/post/image/:postId", AuthMiddleware,postImage)
+app.post("/comment/:postId",AuthMiddleware,commentOnPost)
+app.post("/like/:postId",AuthMiddleware,likePost)
+app.post("/unlike/:postId",AuthMiddleware,unlikePost)
+app.delete("/post/delete/:postId",AuthMiddleware,deletePost)
 app.get("/all/posts", getPosts)
-
-
-
 
 
 

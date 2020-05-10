1dfba19b4d4773341994983a327758c7664ed383
alexmwaura <alexmwaura43@gmail.com>
2020-05-10 13:33:24 +0300

    add database url

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("../config/multi-use.json")
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "portfolio-alex-eb3ef.appspot.com"
+const serviceAccount = require("../config/config.json")
+admin.initializeApp({ 
+    credential: admin.credential.cert(serviceAccount), 
+    storageBucket: "portfolio-alex-eb3ef.appspot.com",
+    databaseURL: "https://portfolio-alex-eb3ef.firebaseio.com"
     
 })
+   
 const db = admin.firestore()
-
-module.exports = {admin, db}
+module.exports = { admin, db }

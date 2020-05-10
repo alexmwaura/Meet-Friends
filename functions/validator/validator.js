91b04eb433ad976a590b19926f0ca06a579b8c24
alexmwaura <alexmwaura43@gmail.com>
2020-05-10 13:34:44 +0300

    update validators

--- functions/validator/validator.js
+++ functions/validator/validator.js
@@ -3,11 +3,17 @@ const isEmpty = (string) => {
   else return false;
 };
 
+const isEmail = (email) => {
+  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email.match(regex)) return true;
+  else return false;
+};
+
 // post validators
 exports.validatePostData = (data) => {
   let errors = {};
 
-  if (isEmpty(data.author)) errors.author = "Must not be empty";
   if (isEmpty(data.title)) errors.title = "Must not be empty";
   if (isEmpty(data.post)) errors.post = "Must not be empty";
 
@@ -28,3 +34,33 @@ exports.postReducers = (data) => {
 
   return postData;
 };
+
+exports.validateSignupData = (user) => {
+  let errors = {};
+
+  if (isEmpty(user.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(user.email)) {
+    errors.email = "Must be a valid email";
+  }
+  if (isEmpty(user.username)) errors.username = "Must not be empty";
+  if (isEmpty(user.password)) errors.password = "Must not be empty";
+  if (user.password !== user.confirmPassword) {
+    errors.confirmPassword = "Password must match";
+  }
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+exports.validateLoginData = (data) => {
+  let errors = {};
+if (isEmpty(data.email)) errors.email = 'Must not be Empty';
+if (isEmpty(data.password)) errors.password = 'Must not be Empty';
+
+return {
+      errors,
+      valid: Object.keys(errors).length  === 0 ? true:false
+  }
+}

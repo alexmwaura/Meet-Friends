92225cebc879119d5e64cafbd884e1f5a5141e02
alexmwaura <alexmwaura43@gmail.com>
2020-05-12 00:27:43 +0300

    modify

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin")
 const serviceAccount = require("../config/config.json")
-admin.initializeApp({ 
-    credential: admin.credential.cert(serviceAccount), 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
     storageBucket: "portfolio-alex-eb3ef.appspot.com",
     databaseURL: "https://portfolio-alex-eb3ef.firebaseio.com"
-    
+
 })
-   
+
 const db = admin.firestore()
 module.exports = { admin, db }

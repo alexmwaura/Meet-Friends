27a03c44ecc5fb3b44f5bebc61c8573372cafbe0
alexmwaura <alexmwaura43@gmail.com>
2020-05-12 23:54:13 +0300

    add custom landing page

---
+++ frontend/src/components/landingPage/landingPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class landingPage extends Component {
+    render() {
+        return (
+            <div className="App">
+      <header className="App-header">
+       Home
+      </header>
+    </div>
+        )
+    }
+}
+
+export default landingPage

06c4bc5ea1c7128fdc30c5630df1a6cfb1ef950a
alexmwaura <alexmwaura43@gmail.com>
2020-05-12 23:56:38 +0300

    add user reducers

---
+++ frontend/src/redux/reducers/userReducers.js
@@ -0,0 +1,18 @@
+import {SET_USER,SET_AUTHENTICATED,SET_UNAUTHENTICATED,LOADING_USER} from "../store/types"
+const initialState = {
+    authenticated: false,
+    loading: false,
+    credentilas: {}
+}
+
+export default function(state = initialState,action) {
+    switch(action.type){
+        case SET_AUTHENTICATED:return{...state,authenticated:true}
+        case SET_UNAUTHENTICATED:return initialState
+        case SET_USER:return{authenticated:true, loading: false,...action.payload}
+        case LOADING_USER:return{...state,loading:true}
+        default: return state
+
+    }
+
+}

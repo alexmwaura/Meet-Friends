04228fa90b4edb68ac8948591a859a2e5fbc8307
alexmwaura <alexmwaura43@gmail.com>
2020-05-20 02:28:34 +0300

    add meta panel section

---
+++ frontend/src/components/metaPanel/metaPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class metaPanel extends Component {
+    render() {
+        return (
+            <div>
+              Meta panel 
+            </div>
+        )
+    }
+}
+
+export default metaPanel

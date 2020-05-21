f81e9ef858db2e4a2e627fc3e437e6002898629a
alexmwaura <alexmwaura43@gmail.com>
2020-05-22 00:26:52 +0300

    update redux types,actions,reducers

--- frontend/src/redux/actions/actions.js
+++ frontend/src/redux/actions/actions.js
@@ -1,66 +1,103 @@
-import { LOADING_UI, SET_ERRORS, SET_AUTHENTICATED, SET_USER, STOP_LOADING_UI,SET_CURRENT_CHANNEL } from "../store/types"
-import firebase from "../../Auth/firebase"
-import axios from "axios"
-import jwt_decode from "jwt-decode"
+import {
+  LOADING_UI,
+  SET_ERRORS,
+  SET_AUTHENTICATED,
+  SET_PRIVATE_CHANNEL,
+  SET_CURRENT_CHANNEL,
+} from "../store/types";
+import firebase from "../../Auth/firebase";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import config from "../../config/config";
 
 export const login = (email, password) => (dispatch, next) => {
-  firebase.auth().signInWithEmailAndPassword(email, password).then(data => {
-    if (data) {
-      const {email} = data.user
-      dispatch(getCurrentUser(email))
-    }
-  })
-    .catch((err) => {
-      dispatch({
-        type: SET_ERRORS,
-        payload: err
-      })
-    });
-}
-
-
-
-
-export const multiAuth = (provider, scope) => (dispatch) => {
-  provider.addScope(`${scope[0]}`);
-  if (scope[1]) provider.addScope(`${scope[1]}`)
-  firebase.auth().signInWithPopup(provider)
-    .then((result) => {
-      if (result.credential) {
-        const { user } = result
-        let token;
-        const { displayName, email, xa } = user
-        token = xa
-        const { picture, user_id } = jwt_decode(token)
-
-        const userData = {
-          email: email,
-          username: displayName,
-          profileImage: picture
-        }
-        dispatch(getCurrentUser(email))
-        dispatch(googleSignup(userData, user_id))
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((data) => {
+      if (data) {
+        const { email } = data.user;
+        dispatch(registerUser(data))
+        
       }
     })
     .catch((err) => {
       dispatch({
         type: SET_ERRORS,
-        payload: err
-      })
+        payload: err,
+      });
     });
+};
 
-}
 
+export const multiAuth = (provider, scope) => (dispatch) => {
+  provider.addScope(`${scope[0]}`);
+  if (scope[1]) provider.addScope(`${scope[1]}`);
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      if (result.credential) {
+        const { user } = result;
+        let token;
+        const { displayName, email, xa } = user;
+        token = xa;
+        const { picture, user_id } = jwt_decode(token);
+
+        const userData = {
+          email: email,
+          username: displayName,
+          profileImage: picture,
+        };
+        dispatch(googleSignup(userData, user_id));
+        dispatch(registerUser(result))
+      }
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err,
+      });
+    });
+};
 
 export const authenticateGoogle = () => (dispatch) => {
-  dispatch({ type: LOADING_UI })
-  const provider = new firebase.auth.GoogleAuthProvider()
-  let scope = ["profile", "email"]
-  dispatch(multiAuth(provider, scope))
+  dispatch({ type: LOADING_UI });
+  const provider = new firebase.auth.GoogleAuthProvider();
+  let scope = ["profile", "email"];
+  dispatch(multiAuth(provider, scope));
+};
 
-}
+export const registerUser = (createdUser) => {
+  const defaultProfileImage = "profile.png";
+  const defaultCoverImage = "download.png";
+  const userId = firebase.auth().currentUser.uid;
+  console.log(userId)
+  const regUser = firebase.database().ref("users");
+  const userRef = firebase
+    .database()
+    .ref("/users/" + userId)
+    .once("value")
+    .then((snapshot) => {
+      console.log(snapshot.val())
+      if (snapshot.val() !== null) {
 
-
+        return {
+          type: SET_ERRORS,
+          payload: { email: `${snapshot.val().email} exists` },
+        };
+      } else {
+        console.log(createdUser)
+        return regUser.child(createdUser.user.uid).set({
+          email: createdUser.user.email,
+          username: createdUser.user.displayName,
+          profileImage: createdUser.user.photoURL,
+          coverImage: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${defaultCoverImage}?alt=media`,
+          userId: userId
+        });
+      }
+    });
+};
 
 // export const authenticateFaceBook = () => (dispatch) => {
 //   dispatch({type: LOADING_UI})
@@ -71,38 +108,36 @@ export const authenticateGoogle = () => (dispatch) => {
 // }
 
 export const authenticateGithub = () => (dispatch) => {
-  dispatch({ type: LOADING_UI })
-  const provider = new firebase.auth.GithubAuthProvider()
-  let scope = ["repo"]
+  dispatch({ type: LOADING_UI });
+  const provider = new firebase.auth.GithubAuthProvider();
+  let scope = ["repo"];
   // let email = "email"
-  dispatch(multiAuth(provider, scope))
-}
-
+  dispatch(multiAuth(provider, scope));
+};
 
 export const googleSignup = (userData, userId) => (dispatch) => {
-  dispatch({ type: LOADING_UI })
-  axios.post(`/google/${userId}`, userData)
-    .then(res => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post(`/google/${userId}`, userData)
+    .then((res) => {
       if (res.data) {
-        return
+        return;
       }
     })
     .catch((err) => {
       dispatch({
         type: SET_ERRORS,
-        payload: err
-      })
+        payload: err,
+      });
     });
-
-}
+};
 
 export const loginUser = (userData) => (dispatch) => {
   dispatch({ type: LOADING_UI });
   axios
     .post("/login", userData)
     .then(() => {
-      dispatch(login(userData.email, userData.password))
-
+      dispatch(login(userData.email, userData.password));
     })
     .catch((err) => {
       dispatch({
@@ -113,12 +148,12 @@ export const loginUser = (userData) => (dispatch) => {
 };
 
 export const signupUser = (newUserData) => (dispatch) => {
-
   dispatch({ type: LOADING_UI });
   axios
     .post("/signup", newUserData)
     .then(() => {
       dispatch(login(newUserData.email, newUserData.password));
+      
     })
     .catch((err) => {
       dispatch({
@@ -128,33 +163,29 @@ export const signupUser = (newUserData) => (dispatch) => {
     });
 };
 
-
 export const setAuthenticatedUser = (user) => {
   return {
     type: SET_AUTHENTICATED,
-    payload: user
-  }
-}
+    payload: user,
+  };
+};
 
-export const getCurrentUser = (email) => (dispatch) => {
-  dispatch({type: LOADING_UI})
-  axios.get(`/user/${email}`).then(user => {
-    if(user){
-      dispatch({ type: SET_USER, payload: user.data })
-    }
-  }).then(()=> {
-    dispatch({type: STOP_LOADING_UI})
-  })
-  .catch((err) => {
-   console.log(err)
-  })
-}
 
-export const setCurrentChannel = (channel)=> (dispatch)=>{
+
+export const setCurrentChannel = (channel) => (dispatch) => {
   dispatch({
     type: SET_CURRENT_CHANNEL,
-    payload:{
-      currentChannel: channel
-    }
-  })
+    payload: {
+      currentChannel: channel,
+    },
+  });
+};
+
+export const setPrivateChannel = isPrivateChannel => {
+  return {
+      type: SET_PRIVATE_CHANNEL,
+      payload: {
+          isPrivateChannel
+      }
+  }
 }
--- frontend/src/redux/reducers/reducers.js
+++ frontend/src/redux/reducers/reducers.js
@@ -1,35 +1,50 @@
-import {SET_GOOGLE_USER,SET_AUTHENTICATED,SET_UNAUTHENTICATED,SET_USER, SET_CURRENT_CHANNEL} from "../store/types"
+import {
+  SET_GOOGLE_USER,
+  SET_AUTHENTICATED,
+  SET_UNAUTHENTICATED,
+  SET_PRIVATE_CHANNEL,
+  SET_CURRENT_CHANNEL,
+} from "../store/types";
 const initialState = {
-    authenticated: false,
-    authenticatedUser: null,
-    currentUser: null
-}
+  authenticated: false,
+  authenticatedUser: null,
+};
 
-export const userReducer = (state = initialState,action) => {
-    switch(action.type){
-        case SET_AUTHENTICATED:return{...state,authenticated:true,authenticatedUser: action.payload}
-        case SET_USER: return{ ...state,currentUser: action.payload}
-        case SET_UNAUTHENTICATED:return {...state }
-        case SET_GOOGLE_USER:return{...state,authenticated:true,...action.payload}
-        default: return state
-
-    }
-
-}
-
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_AUTHENTICATED:
+      return {
+        ...state,
+        authenticated: true,
+        authenticatedUser: action.payload,
+      };
+    case SET_UNAUTHENTICATED:
+      return { ...initialState };
+    case SET_GOOGLE_USER:
+      return { ...state, authenticated: true, ...action.payload };
+    default:
+      return state;
+  }
+};
 
 const initialChannelState = {
-    currentChannel: null,
-}
+  currentChannel: null,
+  isPrivateChannel: false,
+};
 
-export const currentChannelReducer =(state = initialChannelState, action) => {
-    switch(action.type){
-        case SET_CURRENT_CHANNEL: return{...state,currentChannel: action.payload.currentChannel}
-        default: return state
-    }
-}
-
-
+export const currentChannelReducer = (state = initialChannelState, action) => {
+  switch (action.type) {
+    case SET_CURRENT_CHANNEL:
+      return { ...state, currentChannel: action.payload.currentChannel };
+    case SET_PRIVATE_CHANNEL:
+      return {
+        ...state,
+        isPrivateChannel: action.payload.isPrivateChannel,
+      };
+    default:
+      return state;
+  }
+};
 
 
 

55090e1f57b700c99b5d81e1ea737af94220097e
alexmwaura <alexmwaura43@gmail.com>
2020-05-22 00:26:26 +0300

    modify theme

---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,19 @@
+export default {
+    palette: {
+      primary: {
+        light: "#33c9dc",
+        main: "#607d8b",
+        dark: "#795548",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#ff6333",
+        main: "#424242",
+        dark: "#795548",
+        contrastText: "#fff",
+      },
+      
+    },
+    
+  };
+  

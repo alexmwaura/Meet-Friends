5952a5800e9db27a2f09e50b0f74a0426c72102a
alexmwaura <alexmwaura43@gmail.com>
2020-05-22 00:26:05 +0300

    modify component

---
+++ frontend/src/components/sidePanel/sidePanel.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {Menu} from "semantic-ui-react"
+import UserPanel from "../userPanel/userpanel"
+import "../landingPage/index.css"
+import Channels from "../channels/Channels"
+import DirectMessages from "../directMessages/directMessages"
+
+ class sidePanel extends Component {
+
+    render() {
+        return (
+
+           
+        
+            <Menu size="large" inverted fixed="left" vertical style={{background: '#607d8b', fontSize: '1.2rem'}}>
+                
+            <UserPanel currentUser={this.props.authenticatedUser} />
+            <br/>
+            <Channels authenticatedUser={this.props.authenticatedUser}/>
+            <DirectMessages 
+            key={this.props.authenticatedUser && this.props.authenticatedUser.uid}
+            user={this.props.authenticatedUser}/>
+           
+         </Menu>
+         
+        )
+    }
+}
+
+
+
+export default sidePanel

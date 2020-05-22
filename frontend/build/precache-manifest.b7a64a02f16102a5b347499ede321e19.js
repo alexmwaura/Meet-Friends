df91089f11c002a2e4d96f69e6a1683dd2a1ead2
alexmwaura <alexmwaura43@gmail.com>
2020-05-23 00:56:15 +0300

    track files

---
+++ frontend/build/precache-manifest.b7a64a02f16102a5b347499ede321e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71a72b93c6a43ec34d2f174283f1053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e1c7ca871a2ef064de",
+    "url": "/static/css/main.cc55a29d.chunk.css"
+  },
+  {
+    "revision": "4e38bc49d6e8a10baf19",
+    "url": "/static/js/2.db9d3fd9.chunk.js"
+  },
+  {
+    "revision": "88c722e346ececb827c93d21b74460ec",
+    "url": "/static/js/2.db9d3fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e1c7ca871a2ef064de",
+    "url": "/static/js/main.21fd04f7.chunk.js"
+  },
+  {
+    "revision": "8872936593df7c3d93f2",
+    "url": "/static/js/runtime-main.76020874.js"
+  }
+]);

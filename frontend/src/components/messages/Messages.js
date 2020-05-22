a38bd9ccb2ac573abf384873eea0e8a707f2c719
alexmwaura <alexmwaura43@gmail.com>
2020-05-22 23:29:34 +0300

    add ref to privateMessages collection and store them

--- frontend/src/components/messages/Messages.js
+++ frontend/src/components/messages/Messages.js
@@ -10,6 +10,7 @@ class Messages extends Component {
   state = {
     privateChannel: this.props.isPrivateChannel,
     messagesRef: firebase.database().ref("messages"),
+    privateMessagesRef: firebase.database().ref("privateMessages"),
     channel: this.props.currentChannel,
     currentUser: this.props.authenticatedUser,
     messages: [],
@@ -27,13 +28,24 @@ class Messages extends Component {
     }
   }
 
+
+
+
+
+  getMessagesRef = () => {
+   const {messagesRef, privateMessagesRef,privateChannel} = this.state
+   return privateChannel ? privateMessagesRef:messagesRef
+  }
+
+
   addListeners = (channelId) => {
     this.addMessageListener(channelId);
   };
 
   addMessageListener = (channelId) => {
     let loadedMessages = [];
-    this.state.messagesRef.child(channelId).on("child_added", (snap) => {
+    const ref = this.getMessagesRef()
+    ref.child(channelId).on("child_added", (snap) => {
       loadedMessages.push(snap.val());
       this.setState({ messages: loadedMessages, loading: false });
     });
@@ -105,7 +117,7 @@ class Messages extends Component {
     }, []);
     this.setState({ searchResults: searchResults });
     setTimeout(() => this.setState({ searchLoading: false }), 1000);
-  };
+  };    
 
   render() {
     const {
@@ -140,7 +152,9 @@ class Messages extends Component {
         <MessageForm
           messagesRef={messagesRef}
           currentChannel={channel}
+          privateChannel={privateChannel}
           currentUser={currentUser}
+          getMessagesRef={this.getMessagesRef}
         />
       </Fragment>
     );

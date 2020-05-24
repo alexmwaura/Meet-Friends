090025cf4af5e8fdcffebfa54025973e2fc7b11e
alexmwaura <alexmwaura43@gmail.com>
2020-05-25 02:05:50 +0300

    add box

--- frontend/src/components/spinner/spinner.js
+++ frontend/src/components/spinner/spinner.js
@@ -1,22 +1,43 @@
 import React,{Fragment} from "react";
 import { Loader, Dimmer } from "semantic-ui-react";
 import {connect} from "react-redux"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
 
 const Spinner = ({user,messages}) => {
  
 
  const {authenticated} = user
   return (
-    <Dimmer active>
+   <Fragment>
      {authenticated ? (
+    
+       
+         <Fragment>
+            {messages.length > 0 ?
+            (<Dimmer active>
+              <Loader size="huge" content="Preparing Chat..." />
+              </Dimmer>)
+            : 
+            <Box>
+               <Typography className="animated pulse" style={{backgroundColor:"#fff", color:"#000", textAlign:"center",height:"54vh"}}>
 
-        <Fragment>
-          {messages.length > 0 ? <Loader size="huge" content="Preparing Chat..." />:<h1 className="animated pulse">Add message</h1>}
-        </Fragment>
+                 <h1 style={{fontFamily:"italic", fontSize:"xx-large"}}>
+                 Add Message
+                 </h1>
+               </Typography>
+            </Box>
+            
+            }
+
+         </Fragment>
+        
+       
      ):(
       <Loader size="huge" content="Please Wait login in..." />
+    
      )}
-    </Dimmer>
+    </Fragment>
   )
 
 }

5e43617d08890f89e6054d67dec212a38dc1070c
alexmwaura <alexmwaura43@gmail.com>
2020-05-26 00:02:18 +0300

    add types

--- frontend/src/redux/store/types.js
+++ frontend/src/redux/store/types.js
@@ -2,13 +2,16 @@
 export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
 export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
 export const SET_GOOGLE_USER = 'SET_GOOGLE_USER';
-export const SET_USER ='SET_USER'
+export const SET_CURRENT_USER ='SET_CURRENT_USER'
 // UI reducer types
 export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
+export const FIRST_LOAD = 'FIRST_LOAD'
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const LOADING_DATA = 'LOADING_DATA';
 export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+export const SET_COLORS = "SET_COLORS";
+
 // Data reducer types
 export const SET_CURRENT_CHANNEL = 'SET_CURRENT_CHANNEL'  
 export const SET_PRIVATE_CHANNEL = 'SET_PRIVATE_CHANNEL'

2459f49d7d84cdcc367bf644334fc8cccdd6885b
alexmwaura <alexmwaura43@gmail.com>
2020-05-26 00:02:05 +0300

    modify

--- frontend/src/redux/reducers/uiReducers.js
+++ frontend/src/redux/reducers/uiReducers.js
@@ -1,15 +1,17 @@
-import {SET_ERRORS,CLEAR_ERRORS,LOADING_UI,STOP_LOADING_UI} from "../store/types"
+import {SET_ERRORS,CLEAR_ERRORS,LOADING_UI,STOP_LOADING_UI,FIRST_LOAD} from "../store/types"
 
 const initialUi = {
     loading: false,
+    firstLoad: "none",
     errors: null
 }
 
-export default function(state=initialUi,action)  {
+export  default function (state=initialUi,action)  {
     switch(action.type){
         case SET_ERRORS: return {...state, loading:false,errors: action.payload}
         case CLEAR_ERRORS: return {...state, loading:false,errors: null}
         case LOADING_UI: return {...state, loading:true}
+        case FIRST_LOAD: return {...state,firstLoad:action.payload}
         case STOP_LOADING_UI: return {...state, loading:false}
         default: return state
     }

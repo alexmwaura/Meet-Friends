6806759f1bf74878836376f1a5437672074072b4
alexmwaura <alexmwaura43@gmail.com>
2020-05-26 00:00:48 +0300

    add display function

--- frontend/src/components/metaPanel/metaPanel.js
+++ frontend/src/components/metaPanel/metaPanel.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import { Segment, Accordion, Header, Icon, Image } from "semantic-ui-react";
+import {
+  Segment,
+  Accordion,
+  Header,
+  Icon,
+  Image,
+  Button,
+  List,
+} from "semantic-ui-react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
 
 class metaPanel extends Component {
   state = {
     channel: this.props.currentChannel,
     activeIndex: 0,
     privateChannel: this.props.isPrivateChannel,
+    firstLoad: this.props.firstLoad
   };
 
   setActiveIndex = (event, titleProps) => {
@@ -15,21 +27,68 @@ class metaPanel extends Component {
     this.setState({ activeIndex: newIndex });
   };
 
+  displayTopPosters = (posts) => (
+    Object.entries(posts)
+      .sort((a,b) =>b[1] -a[1] )
+      .map(([key, val], i) => (
+        <List.Item key={i} className="animated fadeInUp">
+          <Image src={val.avatar} avatar />
+          <List.Content>
+            <List.Header as="a">{key.toLocaleLowerCase()}</List.Header>
+      <List.Description>{val.count} post{val.count > 1 ? "s": ""}</List.Description>
+          </List.Content>
+        </List.Item>
+      ))
+      .slice(0,5).reverse()
+  )
+
+  displayUserInformation =(channel) => (
+   channel && channel.photoURL ? (
+    <Segment>
+      
+    <Header as="h3" attached="top" className="animated fadeInLeft delay-1s">
+      About{"...  @"}{channel.name}
+      </Header>
+      <Accordion>
+      <Accordion.Title
+          active={this.state.activeIndex === 0}
+          index={0}
+          onClick={this.setActiveIndex}
+        >
+          <Icon name="dropdown" />
+          <Icon name="user" />
+          {channel.name} Profile
+        </Accordion.Title>
+
+        <Accordion.Content active={this.state.activeIndex === 0} className="animated fadeInUp">
+        <Image src={channel.photoURL} /> 
+        </Accordion.Content>
+        </Accordion>
+    
+     </Segment>):""
+   
+  )   
+
   render() {
     const { activeIndex, privateChannel, channel } = this.state;
-
-    if (privateChannel || !channel) return null;
+    const { userPost } = this.props;
+  
+    if (privateChannel || !channel) return (
+      <Box display={this.props.firstLoad} >
+      {()=> this.displayUserInformation(channel)}
+       </Box>   
+    );
 
     return (
-      <Segment>
-        <Header as="h3" attached="top">
-          About #{channel.name}
+      <Segment >
+        <Header as="h3" attached="top" className="animated fadeInLeft delay-1s">
+    About{"...  #"}{channel.name}
         </Header>
 
         <Accordion styled attached="true">
           <Accordion.Title
-            active={activeIndex === 0}
-            index={0}
+            active={activeIndex === 2}
+            index={2}
             onClick={this.setActiveIndex}
           >
             <Icon name="dropdown" />
@@ -37,7 +96,7 @@ class metaPanel extends Component {
             Channel Details
           </Accordion.Title>
 
-          <Accordion.Content active={activeIndex === 0}>
+          <Accordion.Content active={activeIndex === 2} className="animated fadeInUp">
             {channel.details}
           </Accordion.Content>
 
@@ -46,18 +105,22 @@ class metaPanel extends Component {
             index={1}
             onClick={this.setActiveIndex}
           >
+
             <Icon name="dropdown" />
             <Icon name="user circle" />
-            Top posters
+            Top 5
           </Accordion.Title>
 
           <Accordion.Content active={activeIndex === 1}>
-            posters
+            <List>
+              
+              
+              { userPost && Object.values(userPost).length > 0 ?  this.displayTopPosters(userPost):<CircularProgress size={20} disableShrink />}</List>
           </Accordion.Content>
 
           <Accordion.Title
-            active={activeIndex === 2}
-            index={2}
+            active={activeIndex === 0}
+            index={0}
             onClick={this.setActiveIndex}
           >
             <Icon name="dropdown" />
@@ -65,11 +128,19 @@ class metaPanel extends Component {
             Created By
           </Accordion.Title>
 
-          <Accordion.Content active={activeIndex === 2}>
-            <Image src={channel.createdBy.avatar} avatar/>
-            {channel.createdBy.name}
+          <Accordion.Content active={activeIndex === 0} className="animated fadeInUp delay-0.5s">
+            <Image src={channel.createdBy.avatar}  />
+            <Typography
+            variant="body1"
+            style={{ opacity: 0.8, fontStyle: "italic", textAlign: "center" }}
+            >
+            {channel.createdBy.name.toLowerCase()}
+            </Typography>
+            
           </Accordion.Content>
         </Accordion>
+
+    
       </Segment>
     );
   }
